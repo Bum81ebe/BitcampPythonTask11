f90874bdaf66b18adbbb20765efe46f095020eec
Bum81ebe <86529395+Bum81ebe@users.noreply.github.com>
2023-03-10 00:09:06 +0400

    Add files via upload

---
+++ Counting.py
@@ -0,0 +1,10 @@
+# counting 
+from tkinter import *
+#asking a question
+
+question = input("Please input any text ")
+length = len(question)
+if length == 0:
+    print("You need to enterText, please")
+else:
+    print(f"Your text is {question} and it has {length} symbols")
